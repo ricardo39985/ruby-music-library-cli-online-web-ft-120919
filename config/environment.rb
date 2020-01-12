e8b8d2eaf9945df3c541eda0187077eba945d7b8
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-12 16:58:56 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -218,11 +218,8 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     choice = gets.chomp.to_i
-    songs = list = Song.all.sort_by {|obj| obj.name}
-    song_name = nil
-    song_artist = nil
-    binding.pry
-
+    songs = list_songs
+    
     puts "Playing #{songs[choice].name} by #{songs[choice].artist.name}"
   end
 
